097d611cc41b74a6586fd98a00ce6cf00af72b46
정현우 <erios5774@gmail.com>
2025-02-25 12:09:43 +0900

    Update news_script.py

--- news_script.py
+++ news_script.py
@@ -1,20 +1,28 @@
 import sqlite3
+import random
 from selenium import webdriver as wb
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+import requests
+from bs4 import BeautifulSoup
 from gensim.summarization import summarize
 from selenium.common.exceptions import NoSuchElementException
+from scipy.linalg import triu
 import re
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 options = Options()
 options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
 options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
+
 
 service = Service('./chromedriver')
 driver = wb.Chrome(service=service, options=options)
